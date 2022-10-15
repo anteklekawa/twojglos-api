9846047b2f5dab33aa1292dffb3d3132cfb9b012
anteklekawa3 <egiku7tyri77wpk7kwryiwfmct7y3wqzb2xe5vrbjwd2kbbbsvtq>
2022-10-16 01:22:17 +0200

    cors_fix

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,15 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors(options);
   await app.listen(8080);
 }
 bootstrap();
