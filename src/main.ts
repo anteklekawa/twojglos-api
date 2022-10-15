b2f27ad76267c27ef9651729957ef19ecaba54f5
anteklekawa3 <egiku7tyri77wpk7kwryiwfmct7y3wqzb2xe5vrbjwd2kbbbsvtq>
2022-10-16 01:26:17 +0200

    s

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const options = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  };
   app.use(cookieParser());
-  app.enableCors(options);
   await app.listen(8080);
 }
 bootstrap();
